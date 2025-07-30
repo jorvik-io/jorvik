7c47748c4228f0fcb12ab93d7dbed6fd420243d0
kleverman <marcuskleverman@gmail.com>
2025-07-30 21:09:59 +0000

    handle no isolation_provider

--- jorvik/storage/__init__.py
+++ jorvik/storage/__init__.py
@@ -1,8 +1,9 @@
 from pyspark.sql import SparkSession
 
-from typing import Optional, Callable, Union, List
+from typing import Optional, Callable, Union
 
 from jorvik.storage.basic import BasicStorage
+from jorvik.storage.isolation_providers import get_isolation_provider
 from jorvik.storage.isolation import IsolatedStorage
 from jorvik.data_lineage.observer import DataLineageLogger
 from jorvik.storage.protocols import Storage
@@ -27,43 +28,19 @@ def configure(
     st = BasicStorage()
     conf = SparkSession.getActiveSession().sparkContext.getConf()
     lineage_log_path = conf.get('io.jorvik.data_lineage.log_path', '')
-    production_context = conf.get('io.jorvik.storage.production_context') or ['main', 'master', 'production', 'prod']
-    production_context = _normalize_contexts(production_context)
+    production_context = conf.get('io.jorvik.storage.production_context', 'main,master,production,prod').split(',')
 
     if track_lineage and lineage_log_path:
         st.register_output_observer(DataLineageLogger(lineage_log_path))
 
     # If there's a isolation provider, check the isolation value to configure IsolatedStorage.
-    if isolation_provider:
-        isolation = isolation_provider()
-        if isolation and isolation.lower() not in production_context:
-            return IsolatedStorage(st, verbose=verbose, isolation_provider=isolation_provider)
+    if isolation_provider is None:
+        isolation_provider = get_isolation_provider()
+
+    isolation = isolation_provider()
+    if isolation and isolation.lower() not in [p.strip().lower() for p in production_context]:
+        return IsolatedStorage(st, verbose=verbose, isolation_provider=isolation_provider)
 
     return st
 
 __all__ = ["Storage", "configure"]
-
-
-def _normalize_contexts(contexts: Union[str, List[str]]) -> List[str]:
-    """
-    Normalize different input formats into a list of comma-separated strings.
-
-    Examples:
-    - 'main, master'           -> ['main', 'master']
-    - ['main, master']         -> ['main', 'master']
-    - ['main', 'master']       -> ['main', 'master']
-
-    Args:
-        contexts (Union[str, List[str]]): The input contexts to normalize.
-
-    Returns:
-        List[str]: A list of individual context strings.
-    """
-    if isinstance(contexts, str):
-        return [c.strip() for c in contexts.split(',') if c.strip()]
-    elif isinstance(contexts, list):
-        result = []
-        for c in contexts:
-            result.extend([s.strip() for s in c.split(',') if s.strip()])
-        return result
-    return []
